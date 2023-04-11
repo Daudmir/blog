27a19f7e13ae5792f77529bb85f3848c01180051
Daudmir <74597831+Daudmir@users.noreply.github.com>
2023-04-11 19:27:21 +0200

    Update index.jsx

--- src/components/post/index.jsx
+++ src/components/post/index.jsx
@@ -9,21 +9,28 @@ function Posts() {
       const res = await fetch(`${process.env.NEXT_PUBLIC_WP_API}/wp/v2/posts`);
       const posts = await res.json();
       setPostData(posts);
+      console.log(posts);
     }
     fetchData();
   }, []);
 
   return (
-    <div>
-      {postData.map((post) => (
-         <Link href={`/posts/${post.slug}`}>
-          <div key={post.id} className="">
-            <h3 className="text-4xl p-3 text-blue-600">
-              {post.title.rendered}
-            </h3>
+    <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8 text-black">
+      <div className="grid gap-6 lg:grid-cols-2 xl:grid-cols-2">
+        {postData.map((post) => (
+          <div key={post.id} className=" bg-gray-100 border border-gray-300 rounded-lg shadow-md">
+            <Link href={`/posts/${post.slug}`}>
+                <div className="h-40 rounded-t-lg overflow-hidden">
+                  <img src={post.featured_media_url} alt={post.title.rendered} className="object-cover w-full h-full" />
+                </div>
+                <div className="p-6">
+                  <h3 className="text-lg font-medium">{post.title.rendered}</h3>
+                <div className="mt-2" dangerouslySetInnerHTML={{__html: post.content.rendered }} />
+                </div>
+            </Link>
           </div>
-          </Link>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
